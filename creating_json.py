159d888acd484366db5d6530e74332f2d40f4fbe
Sp <hemanth921941@gmail.com>
2020-05-06 20:30:10 +0530

    Made changes in formatting the json file

--- creating_json.py
+++ creating_json.py
@@ -1,31 +1,59 @@
 import json
 
-data=[
-	{
-		"file_name": "C:/Users/Ayush/Desktop/c programming/untitled1.py",
-		"object_name": "Cube",
-		"x": 1,
-		"y":5,
-		"z": 1,
-		"Rx": 10,
-		"Ry":20,
-	    "Rz": 30
-	},
-	{ 
+def input_for_variables(n,variable_name):
 
-		"file_name": "C:/Java programming/untitled1.text",
-		"object_name": "Sphere",
-		"x": 123,
-		"y":5112,
-		"z": 23,
-		"Rx": 810,
-		"Ry":323,
-		"Rz": 2
-	}
-]
+	for i in range(n):
+		ele = input()
+		variable_name.append(ele)
 
-j=json.dumps(data)
 
-with open('Data to be stored into.json','w')as f:
+file_names = []
+object_names = []
+x = []
+y = []
+z = []
+rx = []
+ry = []
+rz = []
+data = {}
+final_data = {}
+variable_key = ['File name','Object name','x','y','z','rx','ry','rz']
+
+number_of_elements = int(input('Provide the number of elements : '))
+
+print ("Enter ",number_of_elements," File names : ")
+input_for_variables(number_of_elements,file_names)
+
+print ("Enter ",number_of_elements," Object names : ")
+input_for_variables(number_of_elements,object_names)
+
+print ("Enter ",number_of_elements," values for x : ")
+input_for_variables(number_of_elements,x)
+
+print ("Enter ",number_of_elements," values for y : ")
+input_for_variables(number_of_elements,y)
+
+print ("Enter ",number_of_elements," values for z : ")
+input_for_variables(number_of_elements,z)
+
+print ("Enter ",number_of_elements," values for rx : ")
+input_for_variables(number_of_elements,rx)
+
+print ("Enter ",number_of_elements," values for ry : ")
+input_for_variables(number_of_elements,ry)
+
+print ("Enter ",number_of_elements," values for rz : ")
+input_for_variables(number_of_elements,rz)
+
+for i in range(number_of_elements):
+	variable_values = [file_names[i],object_names[i],x[i],y[i],z[i],rx[i],ry[i],rz[i]]
+	data = dict(zip(variable_key,variable_values))
+	final_data[i+1] = data
+
+print(final_data)
+
+j=json.dumps(final_data,indent = 6)
+
+with open('Data to be stored into 1.json','w')as f:
 	f.write(j)
 	f.close()
